26630b058ed184b96502c0f9ddec952aa884d8f4
joohnyxxz <jessica.maldonado339@gmail.com>
2026-07-20 16:14:18 -0300

    feat: Adiciona tabelas de log, functions de log e triggers de log

---
+++ postgre/05_triggers/create_log_trg.sql
@@ -0,0 +1,38 @@
+-- ===================================================
+-- LOG TRIGGERS
+-- ===================================================
+
+CREATE TRIGGER trg_log_usuario
+AFTER INSERT OR UPDATE OR DELETE
+ON tb_usuario
+FOR EACH ROW EXECUTE FUNCTION fn_log_usuario();
+
+CREATE TRIGGER trg_log_produto
+AFTER INSERT OR UPDATE OR DELETE
+ON tb_produto
+FOR EACH ROW EXECUTE FUNCTION fn_log_produto();
+
+CREATE TRIGGER trg_log_fornecedor
+AFTER INSERT OR UPDATE OR DELETE
+ON tb_fornecedor
+FOR EACH ROW EXECUTE FUNCTION fn_log_fornecedor();
+
+CREATE TRIGGER trg_log_estoque_lote
+AFTER INSERT OR UPDATE OR DELETE
+ON tb_estoque_lote
+FOR EACH ROW EXECUTE FUNCTION fn_log_estoque_lote();
+
+CREATE TRIGGER trg_log_requisicao
+AFTER INSERT OR UPDATE OR DELETE
+ON tb_requisicao
+FOR EACH ROW EXECUTE FUNCTION fn_log_requisicao();
+
+CREATE TRIGGER trg_log_inventario
+AFTER INSERT OR UPDATE OR DELETE
+ON tb_inventario
+FOR EACH ROW EXECUTE FUNCTION fn_log_inventario();
+
+CREATE TRIGGER trg_log_alerta
+AFTER INSERT OR UPDATE OR DELETE
+ON tb_alerta
+FOR EACH ROW EXECUTE FUNCTION fn_log_alerta();
---
+++ postgre/07_functions/rollback/drop_log_functions.sql
@@ -0,0 +1,17 @@
+-- ===================================================
+-- ROLLBACK: DROP LOG FUNCTIONS
+-- ===================================================
+
+DROP TABLE IF EXISTS fn_log_usuario();
+
+DROP TABLE IF EXISTS fn_log_produto();
+
+DROP TABLE IF EXISTS fn_log_fornecedor();
+
+DROP TABLE IF EXISTS fn_log_estoque_lote();
+
+DROP TABLE IF EXISTS fn_log_requisicao();
+
+DROP TABLE IF EXISTS fn_log_inventario();
+
+DROP TABLE IF EXISTS fn_log_alerta();
