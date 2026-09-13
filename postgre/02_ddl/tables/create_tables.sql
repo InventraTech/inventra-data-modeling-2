7fe3f37eac0a969a8a9c9bd456ac831b52bda2c0
dvarakaki <davi.arakaki@hotmail.com>
2026-09-13 01:32:59 -0300

    refactor: [INV2-137] Remove campo role de tb_user

--- postgre/02_ddl/tables/create_tables.sql
+++ postgre/02_ddl/tables/create_tables.sql
@@ -7,7 +7,6 @@ CREATE TABLE tb_user (
 	name VARCHAR(120) NOT NULL,
 	email VARCHAR(150) NOT NULL UNIQUE,
 	password_hash VARCHAR(255) NOT NULL,
-	role VARCHAR(80),
 	id_kitchen INTEGER,
 	id_profile INTEGER NOT NULL,
 	active BOOLEAN NOT NULL DEFAULT true,
