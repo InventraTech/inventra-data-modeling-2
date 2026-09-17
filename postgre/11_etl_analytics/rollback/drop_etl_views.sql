4499233dfe1156216b0320f8d6362dc4d0de017a
joohnyxxz <jessica.maldonado339@gmail.com>
2026-09-17 11:55:19 -0300

    feat: [INV2-140] adiciona ETL com CTE e Window Function

---
+++ postgre/11_etl_analytics/rollback/drop_etl_views.sql
@@ -0,0 +1,9 @@
+-- ---------------------------------------------------
+-- ROLLBACK ETL / ANALYTICS VIEWS
+-- ---------------------------------------------------
+
+DROP VIEW IF EXISTS vw_product_expiration_urgency;
+
+DROP VIEW IF EXISTS vw_category_monthly_requisition_trend;
+
+DROP VIEW IF EXISTS vw_category_stock_balance;
